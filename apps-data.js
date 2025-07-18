51287fe50bb22b0c0565f79598b88dfebd6eeb8b
Duc_hoc_code <sonlyhongduc@gmail.com>
2025-07-18 23:51:40 +0700

    UPLOAD

--- apps-data.js
+++ apps-data.js
@@ -1,14 +1,129 @@
-const appsData = [
-    { name: 'TapSwap', image: 'https://miro.medium.com/v2/resize:fit:1400/1*b2s_1a2c4i2f_Q2jZ1jQZw.png', link: 'https://t.me/tapswap_bot', category: ['Airdrop', 'Game'] },
-    { name: 'Catizen', image: 'https://pbs.twimg.com/profile_images/1792945229143355392/tVer2oI1_400x400.jpg', link: 'https://t.me/catizenbot', category: ['Airdrop', 'Game', 'Niêm yết'] },
-    { name: 'Grass', image: 'https://pbs.twimg.com/profile_images/1785450259845398528/gA2tBEyC_400x400.jpg', link: 'https://app.getgrass.io/', category: ['DePIN'] },
-    { name: 'Hamster Kombat', image: 'https://miro.medium.com/v2/resize:fit:1200/1*dBI7a_J6iV2sYq24aGEsWg.jpeg', link: 'https://t.me/hamster_kombat_bot', category: ['Airdrop', 'Game'] },
-    { name: 'Tonviewer', image: 'https://tonviewer.com/apple-touch-icon.png', link: 'https://tonviewer.com/', category: ['Tiện ích'] },
-    { name: 'DeDust.io', image: 'https://dedust.io/apple-touch-icon.png', link: 'https://dedust.io/', category: ['Niêm yết', 'Tiện ích', 'DEX'] },
-    { name: 'PixelTap', image: 'https://pbs.twimg.com/profile_images/1796527011932147712/eP5eSGPg_400x400.jpg', link: 'https://t.me/pixelversexyzbot', category: ['Airdrop', 'Game'] },
-    { name: 'VIEC Connect', image: 'https://duccodedao.github.io/VIEC/img/logo.png', link: '#', category: ['Tiện ích', 'DePIN'] },
-    { name: 'Blum', image: 'https://miro.medium.com/v2/resize:fit:1400/1*ndEEZdPAL_w-o2s7o2iFdg.png', link: 'http://t.me/BlumCryptoBot/app', category: ['Airdrop', 'Game', 'CEX', 'DEX'] },
-    { name: 'Yescoin', image: 'https://pbs.twimg.com/profile_images/1788572138724790272/4s6aT247_400x400.jpg', link: 'https://t.me/the_yescoin_bot', category: ['Airdrop', 'Game'] },
-    { name: 'Ston.fi', image: 'https://d1j2c9jkfhu70k.cloudfront.net/stonfi_preview_2_dd784e1b53.png', link: 'https://ston.fi/', category: ['Niêm yết', 'Tiện ích', 'DEX'] },
-    { name: 'GetGems', image: 'https://getgems.io/favicon.ico', link: 'https://getgems.io/', category: ['Tiện ích', 'Niêm yết'] },
-];
+
+    // Dữ liệu toàn diện về hệ sinh thái The Open Network (TON), được tổ chức theo từng danh mục.
+    // Các dự án Airdrop được ưu tiên sắp xếp ở đầu.
+
+    // =================================================================================
+    // I. CÁC DỰ ÁN AIRDROP (GAMEFI, SOCIALFI, TAP-TO-EARN)
+    // =================================================================================
+    const airdropApps = [
+        // --- Danh mục: Chuẩn bị Niêm yết ---
+        { "name": "UNICH", "image": "/logo-coin/IMG_20241108_121723_913.jpg", "link": "https://t.me/UnichAirdropBot/start", "category": ["Airdrop", "DePIN"] },
+        { "name": "WCOIN", "image": "/logo-coin/3.jpg", "link": "https://t.me/wcoin_tapbot/wcoin_app?startapp=NTM4NzcyNTEwNA==", "category": ["Airdrop"] },
+        { "name": "$TREA", "image": "/logo-coin/trs.jpg", "link": "https://t.me/treasury_official_bot/app?startapp=5387725104", "category": ["Airdrop"] },
+        { "name": "$FLIX", "image": "/logo-coin/22.jpg", "link": "https://t.me/W3BFLIXBot?start=iv2ac3fc1201", "category": ["Airdrop"] },
+        { "name": "$YES", "image": "/logo-coin/7.jpg", "link": "https://t.me/yescoin_officialbot/app?startapp=v1_prize_ZdaoFU", "category": ["Airdrop"] },
+        { "name": "$CEX", "image": "/logo-coin/1.jpg", "link": "https://t.me/cexio_tap_bot?start=1716270247286630", "category": ["Airdrop"] },
+        { "name": "$DORM", "image": "/logo-coin/23.jpg", "link": "https://t.me/dormint_bot?start=5387725104", "category": ["Airdrop"] },
+        
+
+       
+        { "name": "$CAP", "image": "/logo-coin/photo_5230958794740198125_c.jpg", "link": "https://t.me/DurovCapsBot/caps?startapp=5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$YES", "image": "/logo-coin/5.jpg", "link": "https://t.me/realyescoinbot?start=r_5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$REP", "image": "/logo-coin/rep.jpg", "link": "https://t.me/repcoinbot/rep?startapp=rep_5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$BLUM", "image": "/logo-coin/12.jpg", "link": "http://t.me/BlumCryptoBot/app?startapp=ref_Sn8ogL25Ap", "category": ["Game", "Airdrop", "DEX"] },
+        { "name": "$TONX", "image": "/logo-coin/txd.jpg", "link": "https://t.me/tonxdao_bot?start=ref_5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$DR", "image": "/logo-coin/dropbot.jpg", "link": "https://t.me/drops/app?startapp=ref_BTFPO", "category": ["Game", "Airdrop"] },
+        { "name": "$WTON", "image": "/logo-coin/wt.jpg", "link": "https://t.me/WontonOrgBot/gameapp?startapp=referralCode=I7HQOXXI", "category": ["Game", "Airdrop"] },
+        { "name": "$KR", "image": "/logo-coin/kr.jpg", "link": "https://t.me/krypton_game_bot/app?startapp=v0_rX5CF1p_guXTPhX", "category": ["Game", "Airdrop"] },
+        { "name": "$TM", "image": "/logo-coin/tm.jpg", "link": "https://t.me/tomowalletbot/tomo_wallet_app?startapp=VASRDHI3DQ", "category": ["Game", "Airdrop", "DEX"] },
+        { "name": "$COUB", "image": "/logo-coin/IMG_20240930_101421_266.jpg", "link": "https://t.me/coub/app?startapp=coub__marker_19934812/", "category": ["Game", "Airdrop"] },
+        { "name": "$PTS", "image": "/logo-coin/IMG_20240930_102758_160.jpg", "link": "https://t.me/walbi_ala_bot/walbi?startapp=Y29kZT1jNTI2YjImZGxfaWQ9cmVmZXJyYWxDb2Rl", "category": ["Game", "Airdrop"] },
+        { "name": "$DTC", "image": "/logo-coin/6.jpg", "link": "https://t.me/dotcoin_bot?start=r_5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$ST", "image": "/logo-coin/11.jpg", "link": "https://t.me/Shuttle_ton_bot?start=r_5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$MD", "image": "/logo-coin/21.jpg", "link": "https://t.me/Mdaowalletbot?start=5387725104", "category": ["Game", "Airdrop"] },
+        { "name": "$BM", "image": "/logo-coin/28.jpg", "link": "https://t.me/boom?start=MUnNNVPPWLQ5aYt6k6jQou", "category": ["Game", "Airdrop"] },
+        { "name": "$CR", "image": "/logo-coin/25.jpg", "link": "https://t.me/cryptorank_app_bot/points?startapp=ref_5387725104_", "category": ["Game", "Airdrop"] },
+        { "name": "$HOLD", "image": "/logo-coin/hold.jpg", "link": "https://fintop.io/6nBvAHD", "category": ["Game", "Airdrop"] },
+        { "name": "$VNF", "image": "/logo-coin/vani.jpg", "link": "https://t.me/Vanilla_Finance_Bot/Vanillafinance?startapp=inviteId10141498", "category": ["Game", "Airdrop"] }
+    ];
+
+    // =================================================================================
+    // II. SÀN GIAO DỊCH (EXCHANGES)
+    // =================================================================================
+    const sanDEX = [
+      { 
+        "name": "Ston.fi", 
+        "image": "/logo-coin/stonfi.png", 
+        "link": "https://ston.fi/", 
+        "category": ["DEX", "Tiện ích"] 
+      },
+      { 
+        "name": "DeDust.io", 
+        "image": "/logo-coin/dedust.png", 
+        "link": "https://dedust.io/", 
+        "category": ["DEX", "Tiện ích"] 
+      }
+    ];
+    const sanCEX = [
+      { 
+        "name": "Binance", 
+        "image": "https://s2.coinmarketcap.com/static/img/exchanges/64x64/270.png", 
+        "link": "https://www.binance.info/vi/activity/referral-entry/CPA/together-v3?ref=CPA_00N60370DE", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "Bybit", 
+        "image": "/logo-coin/bybitx.png", 
+        "link": "https://www.bybitglobal.com/invite?ref=4K7A14M", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "OKX", 
+        "image": "https://s2.coinmarketcap.com/static/img/exchanges/64x64/294.png", 
+        "link": "https://www.okx.com/vi/join/94425783", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "KuCoin", 
+        "image": "https://s2.coinmarketcap.com/static/img/exchanges/64x64/311.png", 
+        "link": "https://www.kucoin.com/trade/TON-USDT", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "Gate.io", 
+        "image": "https://s2.coinmarketcap.com/static/img/exchanges/64x64/302.png", 
+        "link": "https://www.gate.com/vi/signup?ref_type=103&ref=AVLMVF0O", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "MEXC", 
+        "image": "/logo-coin/mexc.png", 
+        "link": "https://www.mexc.com/exchange/TON_USDT", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "HTX (Huobi)", 
+        "image": "https://s2.coinmarketcap.com/static/img/exchanges/64x64/102.png", 
+        "link": "https://www.htx.com/en-us/trade/ton_usdt/", 
+        "category": ["CEX"] 
+      },
+      { 
+        "name": "Bitget", 
+        "image": "/logo-coin/bitget.jpg", 
+        "link": "https://share.bitget.com/u/Y1TJB7JX", 
+        "category": ["CEX"] 
+      }
+    ];
+
+    // =================================================================================
+    // III. CƠ SỞ HẠ TẦNG & TIỆN ÍCH (INFRASTRUCTURE & UTILITIES)
+    // =================================================================================
+    const viDienTu = [
+      { "name": "Tonkeeper", "image": "/logo-coin/tonkeeper.png", "link": "https://tonkeeper.com/", "category": ["DEX","Tiện ích"] }
+    ];
+    const trinhKhamPha = [
+      { "name": "Tonviewer", "image": "https://tonviewer.com/apple-touch-icon.png", "link": "https://tonviewer.com/", "category": ["Tiện ích"] }
+    ];
+    const sanNFT = [
+      { "name": "Getgems", "image": "https://getgems.io/favicon-32x32.png", "link": "https://getgems.io/", "category": ["Tiện ích"] }
+    ];
+
+    // --- GỘP TẤT CẢ DỮ LIỆU LẠI VÀ ƯU TIÊN AIRDROP LÊN ĐẦU ---
+    const appsData = [
+        ...airdropApps,
+        ...sanDEX,
+        ...sanCEX,
+        ...viDienTu,
+        ...trinhKhamPha,
+        ...sanNFT
+    ];
